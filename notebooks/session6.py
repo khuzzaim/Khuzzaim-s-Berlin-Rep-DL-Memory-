854740a39d32ddef8a50b4cb49ff06317d9dbabc
Muhammad Khuzzaim Shakeel Khan <muhammad-khuzzaim-shakeel.khan@charite.de>
2026-04-26 18:06:46 +0200

    Final fixes session 6

--- notebooks/session6.py
+++ notebooks/session6.py
@@ -14,40 +14,22 @@
 # ---
 
 # %%
-# %load_ext autoreload
-# %autoreload 2
+# # %load_ext autoreload
+# # %autoreload 2
 
 # %%
 import spacemed
+import matplotlib.pyplot as plt
 
 # %%
 spacemed.__version__
 
 # %%
-import matplotlib.pyplot as plt
-
-# %%
-# Load absorption data from CSV file
-
-# %%
+# Load data
 time, absorption = spacemed.read_pulse("../data/pulse_data.csv")
 
 # %%
-# Importing Functions
-
-import numpy as np
-
-# %%
-# Convert to numpy
-
-# %%
-time = np.array(time)
-absorption = np.array(absorption)
-
-# %%
-# Plot pulse data over time using matplotlib
-
-# %%
+# Plot signal
 plt.plot(time, absorption)
 plt.xlabel("Time")
 plt.ylabel("Absorption")
@@ -55,86 +37,19 @@ plt.title("Absorption over Time")
 plt.show()
 
 # %%
-# Peak Detection
-
-##Peaks correspond to heart beats and are identified as local maxima in the absorption signal.
-
-##Using NumPy arrays allows efficient vectorized comparison of neighboring values to detect peaks.
-
-##A minimum time separation between consecutive peaks is enforced to ensure that detected peaks represent physiological heart beats rather than noise.
-
-# %%
-peak_indices = np.where(
-    (absorption[1:-1] > absorption[:-2]) &
-    (absorption[1:-1] > absorption[2:])
-)[0] + 1
-
-# %%
-# Applying Time Separation
-
-# %%
-peaks = time[peak_indices]
-
-filtered_peaks = [peaks[0]]
-
-for t in peaks[1:]:
-    if t - filtered_peaks[-1] > 0.3:
-        filtered_peaks.append(t)
-
-peaks = np.array(filtered_peaks)
-
-# %%
-print(len(peaks))
+# Use package function
+peaks = spacemed.find_peaks(time, absorption)
 
 # %%
 print("Number of peaks detected:", len(peaks))
 
 # %%
-# Time Difference Between Peaks
-
-# We compute the time difference between consecutive peaks.
-
-# %%
-delta_t = np.diff(peaks)
+# Heart rate calculation
+heart_rate = spacemed.calc_heart_rate(time, peaks)
 
 # %%
-# Heart Rate Calculation
-
-#The heart rate is calculated using:
-
-# H = 60 / ΔT
-
-# where ΔT is the time between peaks.
-
-# %%
-heart_rate = spacemed.calc_heart_rate(peaks)
-
-# %%
-# Heart Rate Plot
-
-# We plot the heart rate over time.
-
-# %%
+# Plot heart rate
 plt.plot(peaks[1:], heart_rate)
 plt.xlabel("time [s]")
 plt.ylabel("heart rate [bpm]")
 plt.show()
-
-# %% [raw]
-# # Result and Discussion
-#
-# The computed heart rate varies over time and reflects the physiological changes in the signal.
-#
-# The values obtained are within a realistic range for heart rate measurements.
-
-# %% [raw]
-# # Conclusion
-#
-# In this session, we:
-# - visualized the absorption signal
-# - identified peaks corresponding to heart beats
-# - computed time intervals between peaks
-# - calculated the heart rate
-# - visualized the heart rate over time
-#
-# This demonstrates basic algorithm design applied to physiological data.
